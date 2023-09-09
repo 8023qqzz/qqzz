085670dc0d531b4dedb9585b0c214f5b830e2725
8023qqzz <111619617+8023qqzz@users.noreply.github.com>
2023-09-09 20:26:40 +0800

    Delete 王者局内道具展示小工具 v3.0.0 directory

--- 王者局内道具展示小工具 v3.0.0/db/migrations.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-from base import engine
-from models import Item, Bjtwitem
-
-if __name__ == '__main__':
-    Item.metadata.create_all(bind=engine)
-    Bjtwitem.metadata.create_all(bind=engine)
